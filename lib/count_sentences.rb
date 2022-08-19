f830c6042ecb9ea16fcf977451232094b36c688b
Thecodingobare <daniel.obare@student.moringaschool.com>
2022-08-19 10:04:01 +0300

    commit tree e7d1b567541c05f8df202639949b373aa31f3cf3

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,22 @@
+20 lines (15 sloc)  276 Bytes
+
 require 'pry'
 
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    self.split(/\.|\?|!/).filter { |sentence| !sentence.empty? }.length
   end
 end
