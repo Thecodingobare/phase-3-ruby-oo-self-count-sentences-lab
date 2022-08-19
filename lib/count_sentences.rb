fec4395cb7d35c0a328cf561cd678af34456b74a
Thecodingobare <daniel.obare@student.moringaschool.com>
2022-08-19 10:08:39 +0300

    Completed

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,30 @@ require 'pry'
 class String
 
   def sentence?
+    if self.end_with?(".")
+      return true
+    else
+      return false
+    end
     
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    sentence_arr = self.split(/[.?!]/)
+   #binding.pry
+    sentence_arr.delete_if {|word| word==""}
+    sentence_arr.count
   end
 end
